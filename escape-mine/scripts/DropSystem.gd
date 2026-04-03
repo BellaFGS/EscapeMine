6e9b1993a6708c64d41cfcf2f22fdbf7a17d926a
BellaFGS <ferreiraisabella1216@gmail.com>
2026-04-03 11:40:05 -0300

    Inimigo base e sprites adicionados

--- escape-mine/scripts/DropSystem.gd
+++ escape-mine/scripts/DropSystem.gd
@@ -7,7 +7,7 @@ func gerar_drop():
 	var roll = rng.randi_range(1, 100)
 
 	if roll <= 5:
-		return preload(res://scenes/items/item_chave.tscn")
+		return preload("res://scenes/items/item_chave.tscn")
 	elif roll <= 35:
 		return preload("res://scenes/items/item_forca.tscn")
 	elif roll <= 70:
