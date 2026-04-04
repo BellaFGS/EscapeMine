86d6ed5119644677b924216c3c50d0d005dfebf5
BellaFGS <ferreiraisabella1216@gmail.com>
2026-04-04 12:00:18 -0300

    Animação - Player
    Aplicação de animação de ataque no player e reajustes de herança no character

--- escape-mine/scripts/Character.gd
+++ escape-mine/scripts/Character.gd
@@ -1,18 +1,74 @@
 extends CharacterBody2D
 
+@onready var anim = get_node_or_null("Animator")
 var vida = 100
 var dano = 10
 var speed = 100
 
+var ultima_direcao = "down"
+var esta_morto = false
+var is_attack = false
+
 func mover(direcao):
+	if esta_morto:
+		return
+	
 	velocity = direcao * speed
 	move_and_slide()
 
+	atualizar_animacao(direcao)
+
+# 🔥 CONTROLE DE ANIMAÇÃO
+func atualizar_animacao(direcao):
+	if anim == null:
+		return
+	if anim == null or is_attack:
+		return
+	if direcao == Vector2.ZERO:
+		anim.play("idle")
+		return
+	# Define direção
+	if abs(direcao.x) > abs(direcao.y):
+		if direcao.x > 0:
+			ultima_direcao = "right"
+		else:
+			ultima_direcao = "left"
+	else:
+		if direcao.y > 0:
+			ultima_direcao = "down"
+		else:
+			ultima_direcao = "up"
+	
+	#anim.play("walk_" + ultima_direcao)
+
+# ⚔️ ATAQUE (você chama isso quando quiser)
+func atacar():
+	if esta_morto or is_attack:
+		return
+	
+	is_attack = true
+	anim.play("attack_" + ultima_direcao)
+
+# ❤️ DANO
 func receber_dano(valor):
+	if esta_morto:
+		return
+	
 	vida -= valor
 	
 	if vida <= 0:
 		morrer()
 
+# 💀 MORTE
 func morrer():
+	esta_morto = true
+	velocity = Vector2.ZERO
+	
+	#anim.play("death")
+	
+	#await anim.animation_finished
 	queue_free()
+
+func _on_Animator_animation_finished():
+	if anim.animation.begins_with("attack"):
+		is_attack = false
--- escape-mine/scripts/player.gd
+++ escape-mine/scripts/player.gd
@@ -1,14 +1,16 @@
 extends "res://scripts/Character.gd"
 
-
 func _ready():
 	speed = 200
+	
 
 func _physics_process(delta):
 	var direcao = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	).normalized()
+	if Input.is_action_just_pressed("attack") and not is_attack:
+		atacar()
 
 	mover(direcao)
 
@@ -18,3 +20,8 @@ func morrer():
 func pegar_item(item):
 	item.aplicar(self)
 	item.queue_free()
+
+
+func _on_animator_animation_finished(anim_name: StringName) -> void:
+	if anim_name.begins_with("attack"):
+		is_attack = false
