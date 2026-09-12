c2029bd1f39c4d264b53eb8e53dc2e5133a2fdbc
Thomas-1610 <thomaslcl.1610@gmail.com>
2026-09-12 20:32:53 -0300

    arrumando a tela de tutorial (nao finalizado)

--- escape-mine/scripts/Telas/tela_tutorial.gd
+++ escape-mine/scripts/Telas/tela_tutorial.gd
@@ -1,10 +1,48 @@
 extends CanvasLayer
 
-func _on_btn_tutorial_2_pressed() -> void:
+var pagina_atual := 0
+
+var titulos := [
+	"COMANDOS BÁSICOS",
+	"COMANDOS DE ATAQUE",
+	"HUD"
+]
+
+@onready var titulo: Label = $"HBoxContainer/Nome da tela"
+@onready var carrossel: TabContainer = $Carrossel
+
+
+func _ready() -> void:
+	pagina_atual = 0
+	carrossel.current_tab = pagina_atual
+	atualizar_pagina()
+
+
+func atualizar_pagina() -> void:
+	carrossel.current_tab = pagina_atual
+	titulo.text = titulos[pagina_atual]
+
+
+func _on_avançar_pressed() -> void:
+	print("CLICOU EM AVANÇAR")
+
 	AudioManager.tocar_sfx("click")
-	GameFacade.abrir_tutorial_2()
+
+	if pagina_atual < titulos.size() - 1:
+		pagina_atual += 1
+		atualizar_pagina()
 
 
-func _on_button_pressed() -> void:
+func _on_retroceder_pressed() -> void:
+	print("CLICOU EM RETROCEDER")
+
+	AudioManager.tocar_sfx("click")
+
+	if pagina_atual > 0:
+		pagina_atual -= 1
+		atualizar_pagina()
+
+
+func _on_sair_pressed() -> void:
 	AudioManager.tocar_sfx("click")
 	GameFacade.voltar_menu()
